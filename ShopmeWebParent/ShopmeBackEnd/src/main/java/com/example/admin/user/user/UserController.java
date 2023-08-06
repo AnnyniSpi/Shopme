859f9b79826dec9ca85c224f4dacd9a300ae4ca7
Anna <annaspi333@gmail.com>
2023-08-06 15:45:17 +0300

    Add function for User Form in User Service and User Controller

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/example/admin/user/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/example/admin/user/user/UserController.java
@@ -1,10 +1,13 @@
 package com.example.admin.user.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,9 +27,24 @@ public class UserController {
 
     @GetMapping("/users/new")
     public String newUser(Model model){
+        List<Role> listRoles = service.listRoles();
         User user = new User();
+        user.setEnabled(true);
+
+        model.addAttribute("listRoles", listRoles);
         model.addAttribute("user", user);
 
+
         return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes){
+        System.out.println(user);
+        service.save(user);
+
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully!");
+
+        return "redirect:/users";
     }
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/example/admin/user/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/example/admin/user/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.admin.user.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,20 @@ import java.util.List;
 public class UserService {
 
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     public List<User> listAll(){
-        return (List<User>) repo.findAll();
+        return (List<User>) userRepository.findAll();
+    }
+
+    public List<Role> listRoles(){
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
     }
 }
