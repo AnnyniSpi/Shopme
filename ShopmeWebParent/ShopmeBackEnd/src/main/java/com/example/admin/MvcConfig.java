56868e1665f426bc79a56c17d8f8f255f8b3189c
Anna <annaspi333@gmail.com>
2023-08-11 12:02:24 +0300

    Fixed bug in MvcConfig

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/example/admin/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/example/admin/MvcConfig.java
@@ -1,16 +1,18 @@
 package com.example.admin;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
-        String dirName = "users-photo";
+        String dirName = "user-photos";
         Path userPhotosDir = Paths.get(dirName);
 
         String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
